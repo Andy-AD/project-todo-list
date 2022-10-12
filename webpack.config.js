0cc69e94f45b633bfc145a0cd4619b4186882540
Andrii Dovhaliuk <andy87@live.ru>
2022-10-12 11:55:12 +0400

    Update configuration for webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+      },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: {
+            keep: /index\.html$/i
+        }
+    },
+    optimization: {
+        runtimeChunk: 'single',
+      },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },  
+};
