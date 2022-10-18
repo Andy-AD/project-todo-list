9b9e692a91a14125fa66c85cae8d3bd0e8315475
Andrii Dovhaliuk <andy87@live.ru>
2022-10-18 22:30:05 +0400

    Add event listeners for opening and closing modal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import './style.css';
+import * as todoList from './modules/todoList.js';
+import * as display from './modules/displayController.js'
 
-console.log("Hello from index.js")
+
+function todoApp() {
+    display.loadOnStart();
+
+    let currentProject = null;
+    const newTaskButton = document.getElementsByClassName('new-task-button')[0];
+    const closeTaskModal = document.getElementsByClassName('close')[0];
+
+    /* const createTaskForm = document.getElementById('create-task');
+
+    createTaskForm.addEventListener('submit', createTask); */
+
+    newTaskButton.addEventListener('click', display.showModalForTask);
+    closeTaskModal.addEventListener('click', display.closeAndClearModalForTask);
+
+    function createTask(e) {
+        e.preventDefault();
+        console.log(e.target[2].value);
+        let title = e.target[0].value;
+        let dueDate = e.target[1].value;
+        let priority = e.target[2].value;
+        let description = e.target[3].value;
+        todoList.createTask(title, dueDate, priority, description);
+    }
+}
+
+
+todoApp();
