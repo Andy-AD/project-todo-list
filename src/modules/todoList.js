a4b5f1dc7b073aec1eb77cea98bcc675a38bddbb
Andrii Dovhaliuk <andy87@live.ru>
2022-10-26 15:22:52 +0400

    Configure localStorage for storing tasks and projects

--- src/modules/todoList.js
+++ src/modules/todoList.js
@@ -1,7 +1,10 @@
-import {isSameWeek, format} from 'date-fns';
+import { isSameWeek, format } from 'date-fns';
 import { v4 as uuidv4 } from 'uuid';
 
-let taskList = [
+let taskList = getItemsFromStorage('tasks');
+let projects = getItemsFromStorage('projects');
+
+/* let taskList = [
     {title: 'Clean house', dueDate: '2023-08-04', priority: 'medium',id: 'kadlfkhadf123', description: 'Throw before Mom comes home', projectName: 'Household'},
     {title: 'Do homework', dueDate: '2023-03-04', priority: 'high',id: 'kadlfkhadf124', description: 'Do it properly, concentrate!', projectName: 'School'},
     {title: 'Change password', dueDate: '2022-12-04', priority: 'low',id: 'kadlfkhadf125', description: 'Proper password', projectName: 'Work'}
@@ -9,16 +12,22 @@ let taskList = [
 
 let projects = [
     'Household', 'School', 'Work'
-];
+]; */
+
+function onChange() {
+    populateStorage(projects, taskList);
+}
 
 function addTask(title, dueDate, priority, description, projectName) {
     let id = uuidv4();
-    let task = {id,title, dueDate, priority, description, projectName};
+    let task = { id, title, dueDate, priority, description, projectName };
     taskList.push(task);
+    onChange();
 }
 
 function addProject(name) {
     projects.push(name);
+    onChange();
 }
 
 function getProjects() {
@@ -27,12 +36,13 @@ function getProjects() {
 
 function deleteProject(projectToDelete) {
     let indexToDelete = projects.findIndex(project => project === projectToDelete);
-    projects.splice(indexToDelete,1);
+    projects.splice(indexToDelete, 1);
     taskList.forEach(task => {
         if (task.projectName === projectToDelete) {
             deleteTask(task.id);
         }
     })
+    onChange();
 }
 
 function getOneTask(id) {
@@ -47,13 +57,13 @@ function getTasks(view, project) {
         return tasksToday;
     }
     if (view === 'week') {
-        let tasksForWeek = taskList.filter(task => isSameWeek(new Date() ,new Date(task.dueDate)));
+        let tasksForWeek = taskList.filter(task => isSameWeek(new Date(), new Date(task.dueDate)));
         return tasksForWeek;
     }
     if (view === 'project') {
         let projectTasks = taskList.filter(task => task.projectName === project);
         return projectTasks;
-    }     
+    }
 }
 
 function updateTask(id, details) {
@@ -63,34 +73,55 @@ function updateTask(id, details) {
     taskList[indexToUpdate].priority = details.priority;
     taskList[indexToUpdate].projectName = details.projectName;
     taskList[indexToUpdate].description = details.description;
+    onChange();
 }
 
 function deleteTask(id) {
     let indexToDelete = taskList.findIndex(task => task.id === id);
-    taskList.splice(indexToDelete,1);
+    taskList.splice(indexToDelete, 1);
+    onChange();
 }
 
 function completeTask(id) {
     const taskIndex = taskList.findIndex(task => task.id === id);
     taskList[taskIndex].completed = true;
     taskList[taskIndex].completedDate = format(new Date(), 'yyyy-MM-dd');
+    onChange();
 }
 
 function undoCompleteTask(id) {
     const taskIndex = taskList.findIndex(task => task.id === id);
     taskList[taskIndex].completed = false;
     taskList[taskIndex].completedDate = null;
+    onChange();
+}
+
+function populateStorage(projects, tasks) {
+    localStorage.setItem('projects', JSON.stringify(projects));
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function getItemsFromStorage(itemToGet) {
+    if (itemToGet === 'tasks') {
+        const taskArr = JSON.parse(localStorage.getItem('tasks')) || [];
+        return taskArr;
+    }
+
+    if (itemToGet === 'projects') {
+        const projectArr = JSON.parse(localStorage.getItem('projects')) || [];
+        return projectArr;
+    }
 }
 
 export {
     addTask,
-    getOneTask, 
-    getTasks, 
-    updateTask, 
+    getOneTask,
+    getTasks,
+    updateTask,
     deleteTask,
     addProject,
     getProjects,
     completeTask,
     undoCompleteTask,
     deleteProject
-    };
+};
